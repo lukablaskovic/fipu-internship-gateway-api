96f1b6559dde0e9f74c979adf7679a595e97d441
lukablaskovic <lukablaskovic2000@gmail.com>
2023-10-09 23:41:55 +0200

    CORS

--- app/main.py
+++ app/main.py
@@ -23,13 +23,13 @@ from datetime import datetime
 models.Base.metadata.create_all(bind=engine)
 
 
-"""origins = [
-    "http://localhost:5173",
-]"""
+origins = [
+    "https://fp-fipu-internship-frontend.onrender.com/",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[origins],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
