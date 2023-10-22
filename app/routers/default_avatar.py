9bdfc120a97174fe169a11fbbc5780c15cd7ea87
lukablaskovic <lukablaskovic2000@gmail.com>
2023-10-22 15:44:30 +0200

    Update default_avatar.py

--- app/routers/default_avatar.py
+++ app/routers/default_avatar.py
@@ -1,29 +1,28 @@
 avatar = {
-        "url": "https://baserow-media.ams3.digitaloceanspaces.com/user_files/VTkKV2AFEGCpkNmgkcKEn2fEK35Zgajp_4901e8c5528018552e69908cf89c2e0441adc157662aceb474589c8c60ea09a0.png",
-        "thumbnails": {
-            "tiny": {
-                "url": "https://baserow-media.ams3.digitaloceanspaces.com/thumbnails/tiny/VTkKV2AFEGCpkNmgkcKEn2fEK35Zgajp_4901e8c5528018552e69908cf89c2e0441adc157662aceb474589c8c60ea09a0.png",
-                "width": None,
-                "height": 21
-            },
-            "small": {
-                "url": "https://baserow-media.ams3.digitaloceanspaces.com/thumbnails/small/VTkKV2AFEGCpkNmgkcKEn2fEK35Zgajp_4901e8c5528018552e69908cf89c2e0441adc157662aceb474589c8c60ea09a0.png",
-                "width": 48,
-                "height": 48
-            },
-            "card_cover": {
-                "url": "https://baserow-media.ams3.digitaloceanspaces.com/thumbnails/card_cover/VTkKV2AFEGCpkNmgkcKEn2fEK35Zgajp_4901e8c5528018552e69908cf89c2e0441adc157662aceb474589c8c60ea09a0.png",
-                "width": 300,
-                "height": 160
-            }
+    "size": 1874397,
+    "mime_type": "image/png",
+    "is_image": True,
+    "image_width": 5120,
+    "image_height": 5120,
+    "uploaded_at": "2023-10-22T13:43:38.753116Z",
+    "url": "http://localhost/media/user_files/TpUAiwpwcK53NDncZc69s2XJ7JSZeUV6_4901e8c5528018552e69908cf89c2e0441adc157662aceb474589c8c60ea09a0.png",
+    "thumbnails": {
+        "tiny": {
+            "url": "http://localhost/media/thumbnails/tiny/TpUAiwpwcK53NDncZc69s2XJ7JSZeUV6_4901e8c5528018552e69908cf89c2e0441adc157662aceb474589c8c60ea09a0.png",
+            "width": None,
+            "height": 21,
         },
-        "visible_name": "5b9c03d0-f4d2-43c1-9ff0-a79e397d9c5e_avatar.png",
-        "name": "VTkKV2AFEGCpkNmgkcKEn2fEK35Zgajp_4901e8c5528018552e69908cf89c2e0441adc157662aceb474589c8c60ea09a0.png",
-        "size": 1874397,
-        "mime_type": "image/png",
-        "is_image": None,
-        "image_width": 5120,
-        "image_height": 5120,
-        "uploaded_at": "2023-10-08T13:01:25.653711+00:00"
-    }
-
+        "small": {
+            "url": "http://localhost/media/thumbnails/small/TpUAiwpwcK53NDncZc69s2XJ7JSZeUV6_4901e8c5528018552e69908cf89c2e0441adc157662aceb474589c8c60ea09a0.png",
+            "width": 48,
+            "height": 48,
+        },
+        "card_cover": {
+            "url": "http://localhost/media/thumbnails/card_cover/TpUAiwpwcK53NDncZc69s2XJ7JSZeUV6_4901e8c5528018552e69908cf89c2e0441adc157662aceb474589c8c60ea09a0.png",
+            "width": 300,
+            "height": 160,
+        },
+    },
+    "name": "TpUAiwpwcK53NDncZc69s2XJ7JSZeUV6_4901e8c5528018552e69908cf89c2e0441adc157662aceb474589c8c60ea09a0.png",
+    "original_name": "VTkKV2AFEGCpkNmgkcKEn2fEK35Zgaj...57662aceb474589c8c60ea09a0.png",
+}
